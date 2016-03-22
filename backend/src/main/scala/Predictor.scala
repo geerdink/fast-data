11664baab96883d73a7655f3d86340d88a4aab83
geerdink <bas@gdink.com>
2016-03-22 21:48:42 +0100

    Added Predictor class with dummy function: HelloWorld, to be called from rule engine.

---
+++ backend/src/main/scala/Predictor.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by Bas on 22-3-2016.
+  */
+object Predictor {
+
+  def HelloWorld(input: String): Unit = {
+
+    // does not do anything yet;
+    // TODO: hello world scenario = push input string through Spark engine, store results in Cassandra
+  }
+}
