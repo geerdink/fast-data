0d617f59d064f93f439b3c0c3d2218e17c34e1bc
Jaap (J.) de Koning <jaap.de.koning@ing.nl>
2016-03-22 20:07:22 +0100

    updated some links

--- frontend/browser/app/js/services.js
+++ frontend/browser/app/js/services.js
@@ -9,15 +9,17 @@ factory('restAPIservice', function($http) {
     restAPI.getNames = function() {
         return $http({
             //method: 'JSONP',
+            method: 'GET',
             //url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
             //url: 'http://localhost:8080/customer?callback=JSON_CALLBACK'
-            url: 'http://localhost:8080/customer/'
+            url: 'http://172.16.33.169:8080/customer/'
         });
     }
 
     restAPI.getMe = function(theName) {
         return $http({
             //method: 'JSONP',
+            method: 'GET',
             //url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
             //url: 'http://localhost:8080/customer?callback=JSON_CALLBACK'
             url: 'http://172.16.33.16:8085/hello/' + theName
