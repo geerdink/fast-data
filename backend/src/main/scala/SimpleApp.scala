74dd613768849b455380a783bf74fde0a7c4b1a7
geerdink <bas@gdink.com>
2016-03-22 22:56:22 +0100

    Ready for deploy to server.

--- backend/src/main/scala/SimpleApp.scala
+++ backend/src/main/scala/SimpleApp.scala
@@ -10,21 +10,21 @@ object SimpleApp {
     val sc = new SparkContext(conf)
     // val sc = new SparkContext("spark://172.13.66.13:8080", "test", conf)
 
-    val logFile = "README.md"
+    //val logFile = "README.md"
 
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(_.contains("a")).count()
-    val numBs = logData.filter(_.contains("b")).count()
+    //val logData = sc.textFile(logFile, 2).cache()
+   // val numAs = logData.filter(_.contains("a")).count()
+    //val numBs = logData.filter(_.contains("b")).count()
 
     // for testing
     //println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
-    //Pi.Calculate(sc)
+    Pi.Calculate(sc)
 
     // Hello World scenario: write something to Cassandra
-    val cas = sc.cassandraTable("sparking", "test")
-    println(cas.count())
+    //val cas = sc.cassandraTable("sparking", "test")
+    //println(cas.count())
 
-
-    sc.stop()
+    // use in local dev mode??
+    // sc.stop()
   }
 }
