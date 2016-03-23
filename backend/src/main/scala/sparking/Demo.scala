3ff9e7507c6687d9335655808060489e012e6def
geerdink <bas@gdink.com>
2016-03-23 01:53:12 +0100

    Separated rules engine from prediction engine (backend). Created misc script file.

--- backend/src/main/scala/sparking/Demo.scala
+++ backend/src/main/scala/sparking/Demo.scala
@@ -1,26 +1,45 @@
 package sparking
 
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import sparking.k_consumer.KafkaConsumerActor
-import sparking.rules.RuleEngine
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
 
-object Demo extends App {
+object Demo {
+  def main(args: Array[String]) {
 
-  implicit val system = ActorSystem("sparking-demo")
+    import org.apache.spark.streaming.kafka._
 
-  val topic = "test"
+    val conf = new SparkConf().setAppName("SparkING").setMaster("local[2]")
+      .set("spark.cassandra.connection.host", "localhost")
+    val ssc = new StreamingContext(conf, Seconds(5))
+    ssc.checkpoint("checkpoint")
 
-  private val alert = system.actorOf(KafkaConsumerActor.props("test", RuleEngine.receive))
+    val kafkaStream = KafkaUtils.createStream(
+      ssc,
+      Map("broker.id" -> "9092", "logs.dir" -> "/tmp/kafka-logs", "zookeeper.connect" -> "localhost:2181"),
+      Map("sparking" -> 1),
+      StorageLevel.MEMORY_ONLY)
 
-  println(topic)
+    val sc = new SparkContext("spark://172.13.66.13:8080", "test", conf)
 
-  val ssc = new StreamingContext(conf, Seconds(5))
+    println("test 123")
 
-  //val stream = ssc.socketStream("localhost", 7777)
+    ssc.start()
+    ssc.awaitTermination()
 
-  //stream.
+    println("...")
 
-  ssc.start()
-  ssc.awaitTermination()
+   // implicit val system = ActorSystem("sparking-demo")
+   // val topic = "test"
+//    val alert = system.actorOf(KafkaConsumerActor.props("test", RuleEngine.receive))
+   // println(topic)
 
+
+    //val stream = ssc.socketStream("localhost", 7777)
+
+    //stream.
+
+    //ssc.start()
+    //ssc.awaitTermination()
+  }
 }
