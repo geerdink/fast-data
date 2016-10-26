77ed7ac2d76f940466ec57c8c2e75d085accee4c
geerdink <bas@gdink.com>
2016-10-26 11:02:45 +0200

    small changes

--- lambda/src/main/scala/etl/StreamingEtl.scala
+++ lambda/src/main/scala/etl/StreamingEtl.scala
@@ -4,9 +4,6 @@ import etl.domain.Customer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-/**
-  * Created by bas on 10/26/16.
-  */
 object StreamingEtl {
   def main(args: Array[String]) {
 
@@ -14,11 +11,11 @@ object StreamingEtl {
     val conf = new SparkConf().setAppName("fast-data").setMaster("local[*]")
     val ssc = new StreamingContext(conf, Seconds(5))
 
+    // extract
     val stream = ssc.fileStream("hdfs://data/customers.csv")
 
-    // get business classes
-    // val customers = customerData.as[Customer].as("CUSTOMERS")
-
+    // transform...
+    // load...
 
     ssc.start() // tell the StreamingContext to start receiving data
     ssc.awaitTermination()  // wait for the job to finish
